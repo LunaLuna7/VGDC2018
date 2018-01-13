ab1658455723befcebdcd671d5d1a810f4e2c86b
LunaLuna7 <thunderphilip@gmail.com>
2018-01-12 22:06:54 -0800

    UPDATE HighScore & GameScore

---
+++ Waifu Warrior/Assets/scripts/coin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coin : MonoBehaviour{
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("hi");
+        Destroy(gameObject);
+    }
+
+}
+
--- Waifu Warrior/Assets/scripts/timer.cs
+++ Waifu Warrior/Assets/scripts/timer.cs
@@ -8,19 +8,42 @@ public class timer : MonoBehaviour {
 
     public Text timerText;
     private float StartTime;
+
+    public Text gamescore;
+    public Text highscore;
+
 	// Use this for initialization
 	void Start () {
         StartTime = Time.time;
-
+        highscore.text = PlayerPrefs.GetFloat("Highscore").ToString("f2");
+        gamescore.text = PlayerPrefs.GetFloat("Gamescore").ToString("f2");
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+
+
+    // Update is called once per frame
+    void Update () {
         float t = Time.time;
 
         string minutes = ((int) t / 60).ToString();
         string seconds = (t % 60).ToString("f2");
         timerText.text = minutes + ':' + seconds;
 
-	}
+        if (t > PlayerPrefs.GetFloat("Highscore", 0))
+        {
+            PlayerPrefs.SetFloat("Highscore", t);
+            highscore.text = timerText.text;
+        }
+        PlayerPrefs.SetFloat("Gamescore", t);
+        gamescore.text = timerText.text;
+    }
+
+    public void Reset()
+    {
+        PlayerPrefs.DeleteKey("Highscore");
+        PlayerPrefs.DeleteKey("Gamescore");
+
+    }
+
+
 }
