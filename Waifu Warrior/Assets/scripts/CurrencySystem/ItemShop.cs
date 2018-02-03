80db801c6555c4c71f8ebada1d8e289e29ae4b98
LuisLunaDevelopment <thunderphilip@gmail.com>
2018-02-03 05:26:32 -0800

    UPDATE Item Shop
    -created currencyManagement
    -created item sprites
    -created UI item shop
    -created scripts for item management

---
+++ Waifu Warrior/Assets/scripts/CurrencySystem/ItemShop.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemShop : MonoBehaviour {
+
+    public static ItemShop itemShop; //create an instance of ItemShop that is static.
+
+    public List<Items> itemList = new List<Items>(); //Creates a Dynamic list that you can resize and add items of type Items script
+
+    public GameObject ItemHolderPrefab; //Where the Item holder object goes
+    public Transform grid;
+
+    void Start()
+    {
+        itemShop = this; //Makes self/this be the static instance at start of game. 
+        FillList();
+    }
+
+    void FillList() //Instantiates an itemholder object into the grid in the UI
+    {
+        for (int i = 0; i < itemList.Count; i++)
+        {
+            GameObject holder = Instantiate(ItemHolderPrefab, grid, false);
+            ItemHolder holderScript = holder.GetComponent<ItemHolder>(); //Gain acces to the stuff in ItemHolder(the individual info of each item).
+
+            holderScript.itemName.text = itemList[i].ItemName;
+            holderScript.itemPrice.text = "$ " + itemList[i].ItemPrice.ToString();
+            holderScript.itemID = itemList[i].ItemID;
+
+            holderScript.buyButton.GetComponent<BuyButton>().itemID = itemList[i].ItemID;
+
+            if (itemList[i].bought == true)
+            {
+                holderScript.itemImage.sprite = itemList[i].boughtSprite;
+            }
+            else
+            {
+                holderScript.itemImage.sprite = itemList[i].unboughtSprite;
+
+            }
+        }
+    }
+}
---
+++ Waifu Warrior/Assets/scripts/CurrencySystem/Items.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable] 
+public class Items{
+
+    //Each item information
+
+    public string ItemName;
+    public int ItemID;
+
+    public Sprite unboughtSprite;
+    public Sprite boughtSprite;
+
+    public int ItemPrice;
+
+    public bool bought;
+}
