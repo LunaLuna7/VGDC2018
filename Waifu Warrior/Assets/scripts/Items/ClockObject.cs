7c04cfebccca6d5dd7fc8e3a1f9c9b4f53496008
LuisLunaDevelopment <thunderphilip@gmail.com>
2018-02-13 01:09:49 -0800

    Update Particles/Menu

---
+++ Waifu Warrior/Assets/scripts/Items/ClockObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockObject : MonoBehaviour {
+
+    public GameObject Clock;
+
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, 5);
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Instantiate(Clock);
+            Destroy(gameObject);
+        }
+    }
+}
